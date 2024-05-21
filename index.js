3869369eeeeb5506bf676e8ae97ca7241e1b32c4
dashevaivan <iivan1415@gmail.com>
2024-05-22 03:28:36 +0700

    COMMIT

--- index.js
+++ index.js
@@ -3,19 +3,20 @@ const axios = require('axios');
 const app = express();
 const port = 3000;
 
-app.get('/api/standings', async (req, res) => {
-    try {
-        const response = await axios.get('https://apiv3.apifootball.com/', {
-            params: {
-                action: 'get_standings',
-                league_id: 194,
-                APIkey: '9c74298f5318dd689108c32545b765a71b32c2eecce0de8ddf4660151c724393'
-            }
-        });
+app.get('/api/standings', (req, res) => {
+    axios.get('https://apiv3.apifootball.com/', {
+        params: {
+            action: 'get_standings',
+            league_id: 194,
+            APIkey: '9c74298f5318dd689108c32545b765a71b32c2eecce0de8ddf4660151c724393'
+        }
+    })
+    .then(response => {
         res.json(response.data);
-    } catch (error) {
+    })
+    .catch(error => {
         res.status(500).send('Error fetching data');
-    }
+    });
 });
 
 app.listen(port, () => {
